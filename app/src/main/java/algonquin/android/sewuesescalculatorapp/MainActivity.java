f8daf0d7d2f028e6a727fd96e463d6fb5f792cef
Sewuese Ayu <sewuesema@gmail.com>
2023-06-27 01:41:27 -0400

    Created simple calculator and enabled basic functions

--- app/src/main/java/algonquin/android/sewuesescalculatorapp/MainActivity.java
+++ app/src/main/java/algonquin/android/sewuesescalculatorapp/MainActivity.java
@@ -3,12 +3,93 @@ package algonquin.android.sewuesescalculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView resultTV, solutionTV;
+    MaterialButton buttonC, buttonBrackOpen, buttonBrackClose;
+    MaterialButton buttonDivide, buttonMultiply, buttonPlus, buttonMinus, buttonEquals;
+    MaterialButton button0, button1, button2, button3, button4, button5, button6, button7, button8, button9;
+    MaterialButton buttonAC, buttonDot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resultTV = findViewById(R.id.result_tv);
+        solutionTV = findViewById(R.id.solution_tv);
+
+        assignId(buttonC, R.id.button_c);
+        assignId(buttonBrackOpen, R.id.button_open_bracket);
+        assignId(buttonBrackClose, R.id.button_close_bracket);
+        assignId(buttonDivide, R.id.button_divide);
+        assignId(buttonMultiply, R.id.button_multiply);
+        assignId(buttonPlus, R.id.button_plus);
+        assignId(buttonMinus, R.id.button_minus);
+        assignId(buttonEquals, R.id.button_equals);
+        assignId(button0, R.id.button_0);
+        assignId(button1, R.id.button_1);
+        assignId(button2, R.id.button_2);
+        assignId(button3, R.id.button_3);
+        assignId(button4, R.id.button_4);
+        assignId(button5, R.id.button_5);
+        assignId(button6, R.id.button_6);
+        assignId(button7, R.id.button_7);
+        assignId(button8, R.id.button_8);
+        assignId(button9, R.id.button_9);
+        assignId(buttonAC, R.id.button_ac);
+        assignId(buttonDot, R.id.button_dot);
+    }
+
+    void assignId(MaterialButton btn, int id) {
+        btn = findViewById(id);
+        btn.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        MaterialButton button = (MaterialButton) view;
+        String buttonText = button.getText().toString();
+        String datatoCalculate = solutionTV.getText().toString();
+
+        if(buttonText.equals("AC")){
+            solutionTV.setText("");
+            resultTV.setText("0");
+            return;
+        }
+        if(buttonText.equals("=")){
+            solutionTV.setText(resultTV.getText());
+            return;
+        }
+        if(buttonText.equals("C")){
+            datatoCalculate = datatoCalculate.substring(0,datatoCalculate.length()-1);
+        } else {
+            datatoCalculate = datatoCalculate+buttonText;
+        }
+        solutionTV.setText(datatoCalculate);
+        String finalResult = getResult(datatoCalculate);
+
+        if(!finalResult.equals("Error")){
+            resultTV.setText(finalResult);
+        }
+    }
+    String getResult(String data){
+        try {
+            Context context = Context.enter();
+            context.setOptimizationLevel(-1);
+            Scriptable scriptable = context.initStandardObjects();
+            String finalResult = context.evaluateString(scriptable, data, "JavaScript", 1, null).toString();
+            if(finalResult.endsWith(".0")){
+                finalResult = finalResult.replace(".0", "");
+            }
+            return finalResult;
+        } catch (Exception e) {
+            return "Error";
+        }
     }
 }
